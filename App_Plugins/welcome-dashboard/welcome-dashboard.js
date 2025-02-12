68aa7e56de42ff41e1c3f9639d515dd2e2a75f47
Mika C <makomitsuki@gmail.com>
2025-02-12 11:23:51 +0000

    dashboard customization ui

--- App_Plugins/welcome-dashboard/welcome-dashboard.js
+++ App_Plugins/welcome-dashboard/welcome-dashboard.js
@@ -1,47 +1,111 @@
-import { css as c, customElement as i, html as d } from "@umbraco-cms/backoffice/external/lit";
-import { UmbLitElement as n } from "@umbraco-cms/backoffice/lit-element";
-var b = Object.getOwnPropertyDescriptor, h = (l, t, s, m) => {
-  for (var e = m > 1 ? void 0 : m ? b(t, s) : t, a = l.length - 1, r; a >= 0; a--)
-    (r = l[a]) && (e = r(e) || e);
-  return e;
-};
-let o = class extends n {
+import { css as p, state as d, customElement as b, html as n, repeat as m } from "@umbraco-cms/backoffice/external/lit";
+import { UMB_CURRENT_USER_CONTEXT as _ } from "@umbraco-cms/backoffice/current-user";
+import { UmbLitElement as y } from "@umbraco-cms/backoffice/lit-element";
+import { UmbUserCollectionRepository as v } from "@umbraco-cms/backoffice/user";
+var f = Object.defineProperty, w = Object.getOwnPropertyDescriptor, h = (e) => {
+  throw TypeError(e);
+}, c = (e, t, r, s) => {
+  for (var a = s > 1 ? void 0 : s ? w(t, r) : t, o = e.length - 1, i; o >= 0; o--)
+    (i = e[o]) && (a = (s ? i(t, r, a) : i(a)) || a);
+  return s && a && f(t, r, a), a;
+}, U = (e, t, r) => t.has(e) || h("Cannot " + r), g = (e, t, r) => (U(e, t, "read from private field"), r ? r.call(e) : t.get(e)), D = (e, t, r) => t.has(e) ? h("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(e) : t.set(e, r), u;
+let l = class extends y {
+  constructor() {
+    super(), this._userData = [], D(this, u, new v(this)), this.consumeContext(_, (e) => {
+      this._observeCurrentUser(e);
+    }), this._getPagedUserData();
+  }
+  async _getPagedUserData() {
+    const { data: e } = await g(this, u).requestCollection();
+    this._userData = (e == null ? void 0 : e.items) ?? [];
+  }
+  async _observeCurrentUser(e) {
+    this.observe(e.currentUser, (t) => {
+      this._currentUser = t;
+    });
+  }
+  _renderUser(e) {
+    if (e)
+      return n`<uui-table-row class="user">
+          <uui-table-cell>${e.name}</uui-table-cell>
+          <uui-table-cell>${e.email}</uui-table-cell>
+          <uui-table-cell>${e.state}</uui-table-cell>
+      </uui-table-row>`;
+  }
   render() {
-    return d`
-          <h1>
-            <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
-            Dashboard
-          </h1>
-          <div>
-            <p>
-              <umb-localize key="welcomeDashboard_bodytext">
-                This is the Backoffice. From here, you can modify the content,
-                media, and settings of your website.
-              </umb-localize>
-            </p>
-            <p>
-              <umb-localize key="welcomeDashboard_copyright">
-                © Ama Usa An 2025
-              </umb-localize>
-            </p>
-          </div>
-        `;
+    var e;
+    return n`
+      <uui-box>
+        <span slot="headline">
+                <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
+                ${((e = this._currentUser) == null ? void 0 : e.name) ?? "Unknown"}!
+            </span>
+        <div>
+          <p>
+           <umb-localize key="welcomeDashboard_bodytext">
+             This is the Backoffice. From here, you can modify the content,
+             media, and settings of your website.
+           </umb-localize>
+          </p>
+          <p>
+            <umb-localize key="welcomeDashboard_copyright">
+              © Ama Usa An 2025
+            </umb-localize>
+          </p>
+        </div>
+        <uui-table id="users-wrapper">
+  				<uui-table-row>
+  					<uui-table-head-cell>Name</uui-table-head-cell>
+  					<uui-table-head-cell>Email</uui-table-head-cell>
+  					<uui-table-head-cell>Status</uui-table-head-cell>
+  				</uui-table-row>
+  				${m(this._userData, (t) => t.unique, (t) => this._renderUser(t))}
+  			</uui-table>
+    </uui-box>
+      `;
   }
 };
-o.styles = [
-  c`
+u = /* @__PURE__ */ new WeakMap();
+l.styles = [
+  p`
       :host {
-        display: block;
-        padding: 24px;
-      }
+  			display: block;
+  			padding: var(--uui-size-layout-1);
+  		}
+
+        #users-wrapper {
+          border: 1px solid lightgray;
+        }
+        .user {
+          padding: 5px 10px;
+        }
+        .user:not(:first-child) {
+          border-top: 1px solid lightgray;
+        }
+
+        uui-table-head-cell {
+			font-weight: bold;
+		}
+
+		.user:hover,
+		.user:focus {
+			cursor: pointer;
+			background-color: var(--uui-color-surface-alt);
+		}
     `
 ];
-o = h([
-  i("my-welcome-dashboard")
-], o);
-const u = o;
+c([
+  d()
+], l.prototype, "_currentUser", 2);
+c([
+  d()
+], l.prototype, "_userData", 2);
+l = c([
+  b("my-welcome-dashboard")
+], l);
+const P = l;
 export {
-  o as MyWelcomeDashboardElement,
-  u as default
+  l as MyWelcomeDashboardElement,
+  P as default
 };
 //# sourceMappingURL=welcome-dashboard.js.map
--- welcome-dashboard/src/welcome-dashboard.element.ts
+++ welcome-dashboard/src/welcome-dashboard.element.ts
@@ -1,37 +1,105 @@
-import { css, html, customElement } from "@umbraco-cms/backoffice/external/lit";
+import { css, html, customElement, state, repeat } from "@umbraco-cms/backoffice/external/lit";
+import { type UmbCurrentUserModel, UMB_CURRENT_USER_CONTEXT, UmbCurrentUserContext } from "@umbraco-cms/backoffice/current-user";
 import { UmbLitElement } from "@umbraco-cms/backoffice/lit-element";
+import { type UmbUserDetailModel, UmbUserCollectionRepository } from '@umbraco-cms/backoffice/user';
 
 @customElement('my-welcome-dashboard')
 export class MyWelcomeDashboardElement extends UmbLitElement {
 
+  @state()
+    private _currentUser?: UmbCurrentUserModel;
+
+   @state()
+    private _userData: Array<UmbUserDetailModel> = [];
+    
+    #userRepository = new UmbUserCollectionRepository(this);
+
+    constructor() {
+        super();
+        this.consumeContext(UMB_CURRENT_USER_CONTEXT, (instance: UmbCurrentUserContext) => {
+            this._observeCurrentUser(instance);
+        });
+        this._getPagedUserData();
+    }
+
+    private async _getPagedUserData() {
+      const { data } = await this.#userRepository.requestCollection();
+      this._userData = data?.items ?? [];
+    }
+
+    private async _observeCurrentUser(instance: typeof UMB_CURRENT_USER_CONTEXT.TYPE) {
+        this.observe(instance.currentUser, (currentUser: UmbCurrentUserModel | undefined) => {
+            this._currentUser = currentUser;
+        });
+    }
+
+    private _renderUser(user: UmbUserDetailModel) {
+      if (!user) return;
+      return html`<uui-table-row class="user">
+          <uui-table-cell>${user.name}</uui-table-cell>
+          <uui-table-cell>${user.email}</uui-table-cell>
+          <uui-table-cell>${user.state}</uui-table-cell>
+      </uui-table-row>`;
+    }
+
     render() {
-        return html`
-          <h1>
-            <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
-            Dashboard
-          </h1>
-          <div>
-            <p>
-              <umb-localize key="welcomeDashboard_bodytext">
-                This is the Backoffice. From here, you can modify the content,
-                media, and settings of your website.
-              </umb-localize>
-            </p>
-            <p>
-              <umb-localize key="welcomeDashboard_copyright">
-                © Ama Usa An 2025
-              </umb-localize>
-            </p>
-          </div>
-        `;
-      }
+      return html`
+      <uui-box>
+        <span slot="headline">
+                <umb-localize key="welcomeDashboard_heading">Welcome</umb-localize>
+                ${this._currentUser?.name ?? 'Unknown'}!
+            </span>
+        <div>
+          <p>
+           <umb-localize key="welcomeDashboard_bodytext">
+             This is the Backoffice. From here, you can modify the content,
+             media, and settings of your website.
+           </umb-localize>
+          </p>
+          <p>
+            <umb-localize key="welcomeDashboard_copyright">
+              © Ama Usa An 2025
+            </umb-localize>
+          </p>
+        </div>
+        <uui-table id="users-wrapper">
+  				<uui-table-row>
+  					<uui-table-head-cell>Name</uui-table-head-cell>
+  					<uui-table-head-cell>Email</uui-table-head-cell>
+  					<uui-table-head-cell>Status</uui-table-head-cell>
+  				</uui-table-row>
+  				${repeat(this._userData, (user) => user.unique, (user) => this._renderUser(user))}
+  			</uui-table>
+    </uui-box>
+      `;
+    }
 
   static styles = [
     css`
       :host {
-        display: block;
-        padding: 24px;
-      }
+  			display: block;
+  			padding: var(--uui-size-layout-1);
+  		}
+
+        #users-wrapper {
+          border: 1px solid lightgray;
+        }
+        .user {
+          padding: 5px 10px;
+        }
+        .user:not(:first-child) {
+          border-top: 1px solid lightgray;
+        }
+
+        uui-table-head-cell {
+			font-weight: bold;
+		}
+
+		.user:hover,
+		.user:focus {
+			cursor: pointer;
+			background-color: var(--uui-color-surface-alt);
+		}
     `,
   ];
 }
