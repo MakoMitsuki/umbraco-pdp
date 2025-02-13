47c74279bb078e1b5f0dcdf222c3297eb2666a51
Mika C <makomitsuki@gmail.com>
2025-02-13 11:42:06 +0000

    suggestions property editor

--- App_Plugins/suggestions/suggestions.js
+++ App_Plugins/suggestions/suggestions.js
@@ -1,66 +1,112 @@
-import { css as d, property as m, state as _, customElement as y, LitElement as f, html as E } from "@umbraco-cms/backoffice/external/lit";
-import { UmbPropertyValueChangeEvent as b } from "@umbraco-cms/backoffice/property-editor";
-var w = Object.defineProperty, x = Object.getOwnPropertyDescriptor, h = (t) => {
+import { LitElement as x, html as y, ifDefined as d, css as C, property as g, state as h, customElement as b } from "@umbraco-cms/backoffice/external/lit";
+import { UmbPropertyValueChangeEvent as E } from "@umbraco-cms/backoffice/property-editor";
+import { UMB_NOTIFICATION_CONTEXT as T } from "@umbraco-cms/backoffice/notification";
+import { UmbElementMixin as M } from "@umbraco-cms/backoffice/element-api";
+import { UMB_MODAL_MANAGER_CONTEXT as w, UMB_CONFIRM_MODAL as O } from "@umbraco-cms/backoffice/modal";
+var A = Object.defineProperty, I = Object.getOwnPropertyDescriptor, c = (t) => {
   throw TypeError(t);
-}, p = (t, e, s, r) => {
-  for (var i = r > 1 ? void 0 : r ? x(e, s) : e, u = t.length - 1, l; u >= 0; u--)
-    (l = t[u]) && (i = (r ? l(e, s, i) : l(i)) || i);
-  return r && i && w(e, s, i), i;
-}, P = (t, e, s) => e.has(t) || h("Cannot " + s), C = (t, e, s) => e.has(t) ? h("Cannot add the same private member more than once") : e instanceof WeakSet ? e.add(t) : e.set(t, s), a = (t, e, s) => (P(t, e, "access private method"), s), n, v, g, c;
-let o = class extends f {
+}, r = (t, e, i, n) => {
+  for (var s = n > 1 ? void 0 : n ? I(e, i) : e, l = t.length - 1, m; l >= 0; l--)
+    (m = t[l]) && (s = (n ? m(e, i, s) : m(s)) || s);
+  return n && s && A(e, i, s), s;
+}, k = (t, e, i) => e.has(t) || c("Cannot " + i), $ = (t, e, i) => e.has(t) ? c("Cannot add the same private member more than once") : e instanceof WeakSet ? e.add(t) : e.set(t, i), u = (t, e, i) => (k(t, e, "access private method"), i), a, _, p, v, f;
+let o = class extends M(x) {
   constructor() {
-    super(...arguments), C(this, n), this.value = "", this._suggestions = [
+    super(), $(this, a), this.value = "", this._suggestions = [
       "You should take a break",
       "I suggest that you visit the Eiffel Tower",
       "How about starting a book club today or this week?",
       "Are you hungry?"
-    ];
+    ], this.consumeContext(w, (t) => {
+      this._modalManagerContext = t;
+    }), this.consumeContext(T, (t) => {
+      this._notificationContext = t;
+    });
+  }
+  set config(t) {
+    this._disabled = t.getValueByAlias("disabled"), this._placeholder = t.getValueByAlias("placeholder"), this._maxChars = t.getValueByAlias("maxChars");
   }
   render() {
-    return E`
-      <uui-input
-        id="suggestion-input"
-        class="element"
-        label="text input"
-        .value=${this.value || ""}
-        @input=${a(this, n, v)}
-      >
-      </uui-input>
-      <div id="wrapper">
-        <uui-button
-          id="suggestion-button"
-          class="element"
-          look="primary"
-          label="give me suggestions"
-          @click=${a(this, n, c)}
+    return y`
+          <uui-input
+            id="suggestion-input"
+            class="element"
+            label="text input"
+            placeholder=${d(this._placeholder)}
+            maxlength=${d(this._maxChars)}
+            .value=${this.value || ""}
+            @input=${u(this, a, _)}
           >
-            Give me suggestions!
-          </uui-button>
-        <uui-button
-          id="suggestion-trimmer"
-          class="element"
-          look="outline"
-          label="Trim text"
-        >
-          Trim text
-        </uui-button>
-      </div>
-    `;
+          </uui-input>
+          <div id="wrapper">
+            <uui-button
+              id="suggestion-button"
+              class="element"
+              look="primary"
+              label="give me suggestions"
+              ?disabled=${this._disabled}
+              @click=${u(this, a, v)}
+            >
+              Give me suggestions!
+            </uui-button>
+            <uui-button
+                id="suggestion-trimmer"
+                class="element"
+                look="outline"
+                label="Trim text"
+                @click=${u(this, a, f)}
+                >
+                Trim text
+            </uui-button>
+          </div>
+        `;
   }
 };
-n = /* @__PURE__ */ new WeakSet();
-v = function(t) {
-  this.value = t.target.value, a(this, n, g).call(this);
+a = /* @__PURE__ */ new WeakSet();
+_ = function(t) {
+  this.value = t.target.value, u(this, a, p).call(this);
 };
-g = function() {
-  this.dispatchEvent(new b());
+p = function() {
+  this.dispatchEvent(new E());
 };
-c = function() {
+v = function() {
   const t = this._suggestions.length * Math.random() | 0;
-  this.value = this._suggestions[t], a(this, n, g).call(this);
+  this.value = this._suggestions[t], u(this, a, p).call(this);
+};
+f = function() {
+  var i, n;
+  if (!this._maxChars) return;
+  if (!this.value || this.value.length <= this._maxChars) {
+    const s = {
+      message: "Nothing to trim!"
+    };
+    (i = this._notificationContext) == null || i.peek("danger", { data: s });
+    return;
+  }
+  const t = this.value.substring(0, this._maxChars), e = (n = this._modalManagerContext) == null ? void 0 : n.open(
+    this,
+    O,
+    {
+      data: {
+        headline: "Trim text",
+        content: `Do you want to trim the text to "${t}"?`,
+        color: "danger",
+        confirmLabel: "Trim"
+      }
+    }
+  );
+  e == null || e.onSubmit().then(() => {
+    var l;
+    this.value = t, u(this, a, p).call(this);
+    const s = {
+      headline: "Text trimmed",
+      message: "You trimmed the text!"
+    };
+    (l = this._notificationContext) == null || l.peek("positive", { data: s });
+  }, null);
 };
 o.styles = [
-  d`
+  C`
           #wrapper {
             margin-top: 10px;
             display: flex;
@@ -71,14 +117,26 @@ o.styles = [
           }
         `
 ];
-p([
-  m({ type: String })
+r([
+  g({ type: String })
 ], o.prototype, "value", 2);
-p([
-  _()
+r([
+  h()
 ], o.prototype, "_suggestions", 2);
-o = p([
-  y("my-suggestions-property-editor-ui")
+r([
+  h()
+], o.prototype, "_disabled", 2);
+r([
+  h()
+], o.prototype, "_placeholder", 2);
+r([
+  h()
+], o.prototype, "_maxChars", 2);
+r([
+  g({ attribute: !1 })
+], o.prototype, "config", 1);
+o = r([
+  b("my-suggestions-property-editor-ui")
 ], o);
 export {
   o as default
--- suggestions/src/suggestions-property-editor-ui.element.ts
+++ suggestions/src/suggestions-property-editor-ui.element.ts
@@ -1,8 +1,11 @@
-import { LitElement, html, css, customElement, property, state } from "@umbraco-cms/backoffice/external/lit";
-import { UmbPropertyEditorUiElement, UmbPropertyValueChangeEvent } from "@umbraco-cms/backoffice/property-editor";
+import { LitElement, html, css, customElement, property, state, ifDefined } from "@umbraco-cms/backoffice/external/lit";
+import { UmbPropertyEditorUiElement, UmbPropertyValueChangeEvent, type UmbPropertyEditorConfigCollection } from "@umbraco-cms/backoffice/property-editor";
+import { UMB_NOTIFICATION_CONTEXT, UmbNotificationDefaultData} from "@umbraco-cms/backoffice/notification";
+import { UmbElementMixin } from "@umbraco-cms/backoffice/element-api";
+import { UMB_MODAL_MANAGER_CONTEXT, UMB_CONFIRM_MODAL,} from "@umbraco-cms/backoffice/modal";
 
 @customElement('my-suggestions-property-editor-ui')
-export default class MySuggestionsPropertyEditorUIElement extends LitElement implements UmbPropertyEditorUiElement {
+export default class MySuggestionsPropertyEditorUIElement extends UmbElementMixin((LitElement)) implements UmbPropertyEditorUiElement {
     @property({ type: String })
     public value = "";
 
@@ -13,6 +16,22 @@ export default class MySuggestionsPropertyEditorUIElement extends LitElement imp
         'How about starting a book club today or this week?',
         'Are you hungry?',
       ];
+
+    @state()
+    private _disabled?: boolean;
+
+    @state()
+    private _placeholder?: string;
+
+    @state()
+    private _maxChars?: number;
+
+    @property({ attribute: false })
+    public set config(config: UmbPropertyEditorConfigCollection) {
+      this._disabled = config.getValueByAlias("disabled");
+      this._placeholder = config.getValueByAlias("placeholder");
+      this._maxChars = config.getValueByAlias("maxChars");
+    }
 
       #onInput(e: InputEvent) {
         this.value = (e.target as HTMLInputElement).value;
@@ -29,38 +48,87 @@ export default class MySuggestionsPropertyEditorUIElement extends LitElement imp
         this.#dispatchChangeEvent();
       }
 
-    render() {
-        return html`
-      <uui-input
-        id="suggestion-input"
-        class="element"
-        label="text input"
-        .value=${this.value || ""}
-        @input=${this.#onInput}
-      >
-      </uui-input>
-      <div id="wrapper">
-        <uui-button
-          id="suggestion-button"
-          class="element"
-          look="primary"
-          label="give me suggestions"
-          @click=${this.#onSuggestion}
-          >
-            Give me suggestions!
-          </uui-button>
-        <uui-button
-          id="suggestion-trimmer"
-          class="element"
-          look="outline"
-          label="Trim text"
-        >
-          Trim text
-        </uui-button>
-      </div>
-    `;
+    _modalManagerContext?: typeof UMB_MODAL_MANAGER_CONTEXT.TYPE;
+    _notificationContext?: typeof UMB_NOTIFICATION_CONTEXT.TYPE;
 
+    constructor() {
+        super();
+        this.consumeContext(UMB_MODAL_MANAGER_CONTEXT, (instance) => {
+            this._modalManagerContext = instance;
+        });
+
+        this.consumeContext(UMB_NOTIFICATION_CONTEXT, (instance) => {
+            this._notificationContext = instance;
+        });
     }
+
+    #onTextTrim() {
+        if (!this._maxChars) return;
+        if (!this.value || (this.value as string).length <= this._maxChars) {
+            const data: UmbNotificationDefaultData = {
+                message: `Nothing to trim!`,
+            };
+            this._notificationContext?.peek("danger", { data });
+            return;
+        }
+
+        const trimmed = (this.value as string).substring(0, this._maxChars);
+        const modalHandler = this._modalManagerContext?.open(this, UMB_CONFIRM_MODAL,
+            {
+                data: {
+                    headline: `Trim text`,
+                    content: `Do you want to trim the text to "${trimmed}"?`,
+                    color: "danger",
+                    confirmLabel: "Trim",
+                }
+            }
+        );
+        modalHandler?.onSubmit().then(() => {
+            this.value = trimmed;
+            this.#dispatchChangeEvent();
+            const data: UmbNotificationDefaultData = {
+                headline: `Text trimmed`,
+                message: `You trimmed the text!`,
+            };
+            this._notificationContext?.peek("positive", { data });
+        }, null);
+    }
+
+      render() {
+        return html`
+          <uui-input
+            id="suggestion-input"
+            class="element"
+            label="text input"
+            placeholder=${ifDefined(this._placeholder)}
+            maxlength=${ifDefined(this._maxChars)}
+            .value=${this.value || ""}
+            @input=${this.#onInput}
+          >
+          </uui-input>
+          <div id="wrapper">
+            <uui-button
+              id="suggestion-button"
+              class="element"
+              look="primary"
+              label="give me suggestions"
+              ?disabled=${this._disabled}
+              @click=${this.#onSuggestion}
+            >
+              Give me suggestions!
+            </uui-button>
+            <uui-button
+                id="suggestion-trimmer"
+                class="element"
+                look="outline"
+                label="Trim text"
+                @click=${this.#onTextTrim}
+                >
+                Trim text
+            </uui-button>
+          </div>
+        `;
+      }
 
     static styles = [
         css`
